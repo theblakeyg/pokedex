3060a81d8567d59af6a8fdad86b8f7cb6536b192
theblakeyg <theblakeyg@gmail.com>
2024-12-27 11:27:18 -0500

    feat: add repl functionality

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := cleanInput(scanner.Text())
+		fmt.Println("Your command was:", text[0])
+	}
+
+	//text, _ := scanner.('\n')
+
+	// fmt.Println("Enter text: ")
+	// text2 := ""
+	// fmt.Scanln(text2)
+	// fmt.Println(text2)
+
+	// ln := ""
+	// fmt.Sscanln("%v", ln)
+	// fmt.Println(ln)
 }
 
 func cleanInput(text string) []string {
 	//split on whitespace
+	text = strings.ToLower(text)
 	words := strings.Fields(text)
-
-	//lowercase word and save back to slice
-	for i, word := range words {
-		words[i] = strings.ToLower(word)
-	}
 
 	return words
 }
